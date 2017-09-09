85e259f173ad4e972675e98c3fc2ffb6127cc02f
Hannah <hannah.munoz92@gmail.com>
2017-09-09 15:27:18 -0700

    lets hope I dont mess up adding the website

--- Assignment0/part0/retry.cpp
+++ Assignment0/part0/retry.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 
 bool sendBoat (int source[2], int dest[2], int size, bool finish);
-void sendCannibal (int numberCannibal, int numberMissonary, int source[2], int dest [2], int size, int side);
-void sendMissionary (int numberCannibal, int numberMissonary, int source[2], int dest [2], int size, int side);
-void loadBoat (int (&source)[2], int (&dest)[2], int cannibals, int missonary);
+bool sendCannibal (int numberCannibal, int numberMissonary, int source[2], int dest [2], int size, int side);
+bool sendMissionary (int numberCannibal, int numberMissonary, int source[2], int dest [2], int size, int side);
 bool eatMissonary ( int cannibals, int missonary);
 void print (int source[2], int dest [2], bool side);
+
+int previousLeft[2];
+int previousRight[2];
 
 int main(int argc, char const *argv[]) {
 
@@ -23,11 +25,11 @@ int main(int argc, char const *argv[]) {
 
     // 0 is left 1 is right
   //  bool check = sendBoat(left, right, size, 0);
-    sendCannibal (0, 0, left, right, size, 0);
+    bool check = sendCannibal (0, 0, left, right, size, 0);
 
-    /*if (!check){
+    if (!check){
       cout << "no solution found." << endl;
-    }*/
+    }
 
   }else{
     cout << "Please put in a boat size" << endl;
@@ -39,65 +41,43 @@ int main(int argc, char const *argv[]) {
 
 
 bool sendBoat (int source[2], int dest[2], int size, bool finish){
-  //check for ending condition
-  /*if (finish){
-    if (source [0] == 3 && source [1] ==3){
-      return true;
-    }
-  }
-  else{
-    if (dest [0] == 3 && dest [1] == 3 ){
-      return true;
-    }
-  }
- bool send;
-  for (int takeCannibal = 0; takeCannibal <= source [0]; takeCannibal ++){
-    for (int takeMissonary = 0; takeMissonary <= source [1]; takeMissonary ++){
-     send = false;
-      if ( takeMissonary + takeCannibal <= size
-           && takeCannibal + takeMissonary > 0){
 
-          //check if valid condition
-            if (eatMissonary (source, takeCannibal, takeMissonary)){
-              cout << takeCannibal << " " << takeMissonary << endl;
-
-              loadBoat (source, dest, takeCannibal, takeMissonary);
-              print (source, dest, finish);
-              send = true;
-            }
-        }
-        if (send){
-          cout << "side" << finish <<endl;
-          sendBoat (dest, source, size, !finish);
-          //return true;
-        }
-      }
-
-    }
-    return false;*/
  if (finish){
    if (source [0] + source [1] == 6){
      return true;
    }
+   if ( previousRight[0] != source [0] &&
+        previousRight[1] != source [1]){
+      previousRight[0] = source [0];
+      previousRight[1] = source [1];
    sendCannibal (0, 0, source, dest, size, finish);
+ }
  }
  else {
    if (source [0] + source [1] != 6){
-     sendCannibal (0, 0, source, dest, size, finish);
+     if ( previousLeft[0] != source [0] &&
+          previousLeft[1] != source [1]){
+        previousLeft[0] = source [0];
+        previousLeft[1] = source [1];
+        sendCannibal (0, 0, source, dest, size, finish);
+    }
    }
  }
 }
 
-void sendCannibal (int numberCannibal, int numberMissonary, int source[2], int dest [2], int size, int side){
+bool sendCannibal (int numberCannibal, int numberMissonary, int source[2], int dest [2], int size, int side){
   if (numberCannibal <= source[0]){
     cout <<"cannibals";
-    sendMissionary (numberCannibal, numberMissonary, source, dest, size, side);
+    if (sendMissionary (numberCannibal, numberMissonary, source, dest, size, side)){
+      return true;
+    }
     sendCannibal (numberCannibal+1, numberMissonary, source, dest, size, side);
+
   }
   cout << "end condition met" << endl;
 }
 
-void sendMissionary(int numberCannibal, int numberMissonary, int source[2], int dest [2], int size, int side){
+bool sendMissionary(int numberCannibal, int numberMissonary, int source[2], int dest [2], int size, int side){
   cout << "try: " <<endl;
   cout << numberCannibal << " " << numberMissonary << endl;
 
@@ -109,7 +89,7 @@ void sendMissionary(int numberCannibal, int numberMissonary, int source[2], int 
             cout << "size";
             cout << side << endl;
            //check if valid condition
-             if (eatMissonary (source[0] -numberCannibal, source [1] - numberMissonary) || eatMissonary (dest[0] + numberCannibal, dest [1] +numberMissonary) ){
+             if (eatMissonary (source[0] -numberCannibal, source [1] - numberMissonary) && eatMissonary (dest[0] + numberCannibal, dest [1] +numberMissonary) ){
                cout <<"eaten";
                cout << numberCannibal << " " << numberMissonary << endl;
                if (numberCannibal <= source [0]
@@ -117,17 +97,13 @@ void sendMissionary(int numberCannibal, int numberMissonary, int source[2], int 
                     cout <<"here";
                      int sourcetemp[2] = {source[0] - numberCannibal, source[1] - numberMissonary};
                      int desttemp [2] = {dest [0] + numberCannibal, dest [1] + numberMissonary};
-                     cout << desttemp[0] << " " <<desttemp [1] << (desttemp [0] + desttemp [1] != 6
+                     cout << desttemp[0] << " " << desttemp [1] << (desttemp [0] + desttemp [1] != 6
                           && !side);
-                     //if (desttemp [0] + desttemp [1] != 6 && !side){
-                    //  loadBoat (source, dest, numberCannibal, numberMissonary);
                     cout << "sent" << endl;
                       print (sourcetemp, desttemp, side);
-                      sendBoat (desttemp, sourcetemp, size, !side);
-                    //}
-                    //else if (){
-
-                    //}
+                      if (sendBoat (desttemp, sourcetemp, size, !side)){
+                        return true;
+                      }
               }
              }
              sendMissionary (numberCannibal, numberMissonary+1, source, dest, size, side);
@@ -136,7 +112,6 @@ void sendMissionary(int numberCannibal, int numberMissonary, int source[2], int 
 }
 
 void print (int source[2], int dest [2], bool side){
-  //cout << takeCannibal << " " << takeMissonary << endl;
   if (!side){
     cout << source [0] << " "<< source [1] << "    " << dest [0]  << " " << dest [1] << endl << endl;
   }
@@ -153,13 +128,4 @@ bool eatMissonary ( int cannibals, int missonary){
     return false;
   }
   return true;
-}
-
-void loadBoat (int (&source)[2], int (&dest)[2], int cannibals, int missonary){
-  source [0] -= cannibals;
-  source [1] -= missonary;
-
-  dest [0] += cannibals;
-  dest [1] += missonary;
-
 }
--- Assignment0/part1/myeval.cpp
+++ Assignment0/part1/myeval.cpp
@@ -3,5 +3,24 @@
 double eval(int * vec){
   double fitness = 10;
 
+  for (int i = 0; i < 100; i+=4 ){
+    if (vec[i] == 1){
+      fitness +=5;
+    }
+    else{
+      fitness -=3;
+    }
+  }
+
+  for (int i = 0; i < 100; i+=2 ){
+    if (vec[i] == 1){
+      fitness +=1;
+    }
+  }
+
+ if (vec [49] == 0){
+   fitness = 60;
+ }
+
   return fitness;
 }
