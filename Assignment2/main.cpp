39d0ed8dafe8692f6a894a75a1f43e064a4cdf57
Hannah <hannah.munoz92@gmail.com>
2017-09-16 11:49:15 -0700

    getting average fitness of 40

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -7,49 +7,65 @@
 
 using namespace std;
 
-int POOL_SIZE = 100;
+static const int POOL_SIZE = 100;
+static const int LENGTH = 150;
 
 double eval(int *vec);
-void fillPool (int p[100][150]);
-void fillVector(int (&vec)[150]);
-void checkFitness (double &b, int (&bv)[150],  int (&cv)[150]);
-void merge (int (&v1)[150],  int (&v2)[150]);
+void fillPool (int p[POOL_SIZE][LENGTH ]);
+void fillVector(int (&vec)[LENGTH ]);
+void roulette(int p[POOL_SIZE][LENGTH], int (&vec)[LENGTH], int &location);
+void checkFitness (double &b, int (&bv)[LENGTH],  int (&cv)[LENGTH]);
+void merge (int (&v1)[LENGTH],  int (&v2)[LENGTH] );
 bool mutate ();
-void print (int (&bvec)[150]);
+void print (int (&bvec)[LENGTH]);
 
 int main()
 {
   srand (time (NULL));
   double bestFitness = 0;
-  int bvec[150];
-  int pool [100][150];
+  int bvec[LENGTH];
+  int pool [POOL_SIZE ][LENGTH];
   fillPool (pool);
 
-  /*for (int iter = 0; iter < 10; iter++){
+  int vec1[LENGTH];
+  int vec2[LENGTH];
+  int loc1;
+  int loc2;
+
+  for (int iter = 0; iter < 10000; iter++){
+    roulette (pool, vec1, loc1);
+    roulette (pool, vec2, loc2);
     checkFitness (bestFitness, bvec, vec1);
     checkFitness (bestFitness, bvec, vec2);
     merge (vec1, vec2);
-  }*/
+    memcpy(pool [loc1], vec1, sizeof(pool [loc1]));
+    memcpy(pool [loc2], vec2, sizeof(pool [loc2]));
+  }
 
   cout << bestFitness << endl;
-  print (pool[0]);
+  print (bvec);
 }
 
-void fillPool (int p[100][150]){
-  for (int i = 0; i < 100; i++){
-    int vec[150];
+void fillPool (int p[POOL_SIZE ][LENGTH]){
+  for (int i = 0; i < POOL_SIZE ; i++){
+    int vec[LENGTH];
     fillVector (vec);
     memcpy(p[i], vec, sizeof(p[i]));
   }
 }
 
-void fillVector(int (&vec)[150]){
-  for(int i = 0; i < 150; i++){
+void fillVector(int (&vec)[LENGTH]){
+  for(int i = 0; i < LENGTH; i++){
     vec[i] = rand()%2;
   }
 }
 
-void checkFitness (double &b, int (&bv)[150], int (&cv)[150]){
+void roulette(int p[POOL_SIZE ][LENGTH], int (&vec)[LENGTH], int &location){
+  location = rand()%POOL_SIZE ;
+  memcpy(vec, p[location], sizeof(vec));
+}
+
+void checkFitness (double &b, int (&bv)[LENGTH], int (&cv)[LENGTH]){
   double currentFitness = eval (cv);
   //cout << "fitness " << currentFitness <<endl;
   if (currentFitness > b){
@@ -58,9 +74,9 @@ void checkFitness (double &b, int (&bv)[150], int (&cv)[150]){
   }
 }
 
-void merge (int (&v1)[150],  int (&v2)[150]){
-  int splitPoint = rand()%150;
-  int temp [150];
+void merge (int (&v1)[LENGTH],  int (&v2)[LENGTH]){
+  int splitPoint = rand()%LENGTH;
+  int temp [LENGTH];
   for (int i = 0; i < splitPoint; i++){
     if (mutate){
       temp[i] = 1 - v1[i];
@@ -68,13 +84,23 @@ void merge (int (&v1)[150],  int (&v2)[150]){
     else{
       temp[i] = v1[i];
     }
-    v1[i] = v2[i];
-    v2[i] = temp[i];
+    if (mutate){
+      v1[i] = 1 - v2[i];
+    }
+    else{
+      v1[i] = v2[i];
+    }
+    if (mutate){
+      v2[i] = 1 - temp[i];
+    }
+    else{
+      v2[i] = temp[i];
+    }
   }
 }
 
-void print (int (&bvec)[150]){
-  for (int i = 0; i < 150; i+=50){
+void print (int (&bvec)[LENGTH]){
+  for (int i = 0; i < LENGTH; i+=50){
     for (int j = 0; j < 50; j++){
       cout << bvec[i+j];
     }
